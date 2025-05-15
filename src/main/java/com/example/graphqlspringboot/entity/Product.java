b13dae01892e108306106a4ba66129874cf024f9
Dinuwan <cdin8619@sysco.com>
2025-05-15 18:33:09 +0530

    feat: updated code:

--- src/main/java/com/example/graphqlspringboot/entity/Product.java
+++ src/main/java/com/example/graphqlspringboot/entity/Product.java
@@ -4,21 +4,23 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@RequiredArgsConstructor
 @Data
 public class Product {
 
     @Id
+    @Getter(AccessLevel.NONE)
+    @Setter(AccessLevel.NONE)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    private String name;
-    private String category;
-    private Float price;
-    private Integer stock;
+
+    @NonNull private String name;
+    @NonNull private String category;
+    @NonNull private Float price;
+    @NonNull private Integer stock;
 }
--- src/main/java/com/example/graphqlspringboot/repository/ProductRepository.java
+++ src/main/java/com/example/graphqlspringboot/repository/ProductRepository.java
@@ -2,9 +2,11 @@ package com.example.graphqlspringboot.repository;
 
 import com.example.graphqlspringboot.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public class ProductRepository extends JpaRepository<Product, Integer> {
 
     public List<Product> findByCategory(String category) {
